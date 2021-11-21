b9d95ce8c8e23754f58aae5f448c226b5d54fcdf
ThorbenSpankas <thorben.spanka@estudiante.uam.es>
2021-11-21 21:23:07 +0100

    jojo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import Web3 from 'web3';
+
 const ethereumButton = document.querySelector('.enableEthereumButton');
 
 ethereumButton.addEventListener('click', () => {
   //Will Start the metamask extension
   ethereum.request({ method: 'eth_requestAccounts' });
 });
+// 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
